af8ab05fda9be5cea7f7dd425609b9c9857349cf
drunkirishcoder <daniel.jin+git@gmail.com>
2013-10-31 12:29:49 -0400

    Add GrowingTextView binding

--- MonoTouch.Controls/ApiDefinition.cs
+++ MonoTouch.Controls/ApiDefinition.cs
@@ -6,61 +6,125 @@ using MonoTouch.UIKit;
 
 namespace MonoTouch.Controls
 {
-	// The first step to creating a binding is to add your native library ("libNativeLibrary.a")
-	// to the project by right-clicking (or Control-clicking) the folder containing this source
-	// file and clicking "Add files..." and then simply select the native library (or libraries)
-	// that you want to bind.
-	//
-	// When you do that, you'll notice that MonoDevelop generates a code-behind file for each
-	// native library which will contain a [LinkWith] attribute. MonoDevelop auto-detects the
-	// architectures that the native library supports and fills in that information for you,
-	// however, it cannot auto-detect any Frameworks or other system libraries that the
-	// native library may depend on, so you'll need to fill in that information yourself.
-	//
-	// Once you've done that, you're ready to move on to binding the API...
-	//
-	//
-	// Here is where you'd define your API definition for the native Objective-C library.
-	//
-	// For example, to bind the following Objective-C class:
-	//
-	//     @interface Widget : NSObject {
-	//     }
-	//
-	// The C# binding would look like this:
-	//
-	//     [BaseType (typeof (NSObject))]
-	//     interface Widget {
-	//     }
-	//
-	// To bind Objective-C properties, such as:
-	//
-	//     @property (nonatomic, readwrite, assign) CGPoint center;
-	//
-	// You would add a property definition in the C# interface like so:
-	//
-	//     [Export ("center")]
-	//     PointF Center { get; set; }
-	//
-	// To bind an Objective-C method, such as:
-	//
-	//     -(void) doSomething:(NSObject *)object atIndex:(NSInteger)index;
-	//
-	// You would add a method definition to the C# interface like so:
-	//
-	//     [Export ("doSomething:atIndex:")]
-	//     void DoSomething (NSObject object, int index);
-	//
-	// Objective-C "constructors" such as:
-	//
-	//     -(id)initWithElmo:(ElmoMuppet *)elmo;
-	//
-	// Can be bound as:
-	//
-	//     [Export ("initWithElmo:")]
-	//     IntPtr Constructor (ElmoMuppet elmo);
-	//
-	// For more information, see http://docs.xamarin.com/ios/advanced_topics/binding_objective-c_libraries
-	//
-}
+	[Model, BaseType (typeof (NSObject))]
+	public partial interface HPGrowingTextViewDelegate
+	{
+		[Export ("growingTextViewShouldBeginEditing:")]
+		bool ShouldBeginEditing(HPGrowingTextView growingTextView);
 
+		[Export ("growingTextViewShouldEndEditing:")]
+		bool ShouldEndEditing(HPGrowingTextView growingTextView);
+
+		[Export ("growingTextViewDidBeginEditing:")]
+		void DidBeginEditing(HPGrowingTextView growingTextView);
+
+		[Export ("growingTextViewDidEndEditing:")]
+		void DidEndEditing(HPGrowingTextView growingTextView);
+
+		[Export ("growingTextView:shouldChangeTextInRange:replacementText:")]
+		bool ShouldChangeTextInRange(HPGrowingTextView growingTextView, NSRange range, string text);
+
+		[Export ("growingTextViewDidChange:")]
+		void DidChange(HPGrowingTextView growingTextView);
+
+		[Export ("growingTextView:willChangeHeight:")]
+		void WillChangeHeight(HPGrowingTextView growingTextView, float height);
+
+		[Export ("growingTextView:didChangeHeight:")]
+		void DidChangeHeight(HPGrowingTextView growingTextView, float height);
+
+		[Export ("growingTextViewDidChangeSelection:")]
+		void DidChangeSelection(HPGrowingTextView growingTextView);
+
+		[Export ("growingTextViewShouldReturn:")]
+		bool ShouldReturn(HPGrowingTextView growingTextView);
+	}
+
+	[BaseType (typeof (UIView))]
+	public partial interface HPGrowingTextView //: UITextViewDelegate
+	{
+		[Export ("maxNumberOfLines")]
+		int MaxNumberOfLines { get; set; }
+
+		[Export ("minNumberOfLines")]
+		int MinNumberOfLines { get; set; }
+
+		[Export ("maxHeight")]
+		int MaxHeight { get; set; }
+
+		[Export ("minHeight")]
+		int MinHeight { get; set; }
+
+		[Export ("animateHeightChange")]
+		bool AnimateHeightChange { get; set; }
+
+		[Export ("animationDuration")]
+		double AnimationDuration { get; set; }
+
+		[Export ("placeholder", ArgumentSemantic.Retain)]
+		string Placeholder { get; set; }
+
+		[Export ("placeholderColor", ArgumentSemantic.Retain)]
+		UIColor PlaceholderColor { get; set; }
+
+		[Export ("internalTextView", ArgumentSemantic.Retain)]
+		UITextView InternalTextView { get; set; }
+
+		[Export ("delegate", ArgumentSemantic.Assign)]
+		HPGrowingTextViewDelegate Delegate { get; set; }
+
+		[Export ("text", ArgumentSemantic.Retain)]
+		string Text { get; set; }
+
+		[Export ("font", ArgumentSemantic.Retain)]
+		UIFont Font { get; set; }
+
+		[Export ("textColor", ArgumentSemantic.Retain)]
+		UIColor TextColor { get; set; }
+
+		[Export ("textAlignment")]
+		UITextAlignment TextAlignment { get; set; }
+
+		[Export ("selectedRange")]
+		NSRange SelectedRange { get; set; }
+
+		[Export ("editable")]
+		bool Editable { [Bind ("isEditable")] get; set; }
+
+		[Since (3,0), Export ("dataDetectorTypes")]
+		UIDataDetectorType DataDetectorTypes { get; set; }
+
+		[Export ("returnKeyType")]
+		UIReturnKeyType ReturnKeyType { get; set; }
+
+		[Export ("keyboardType")]
+		UIKeyboardType KeyboardType { get; set; }
+
+		[Export ("contentInset", ArgumentSemantic.Assign)]
+		UIEdgeInsets ContentInset { get; set; }
+
+		[Export ("isScrollable")]
+		bool IsScrollable { get; set; }
+
+		[Export ("enablesReturnKeyAutomatically")]
+		bool EnablesReturnKeyAutomatically { get; set; }
+
+//		[Export ("becomeFirstResponder")]
+//		bool BecomeFirstResponder();
+//
+//		[Export ("resignFirstResponder")]
+//		bool ResignFirstResponder();
+//
+//		[Export ("isFirstResponder")]
+//		bool IsFirstResponder { get; }
+
+		[Export ("hasText")]
+		bool HasText { get; }
+
+		[Export ("scrollRangeToVisible:")]
+		void ScrollRangeToVisible(NSRange range);
+
+		[Export ("refreshHeight")]
+		void RefreshHeight();
+	}
+}
